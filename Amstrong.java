bfa74fca0f57477c2e9cc7abef257fec534c0160
dhanushanubolu <anuboludhanush@gmail.com>
2025-07-24 11:42:26 +0530

    Create Amstrong.java

---
+++ Amstrong.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class ArmstrongNumber {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+        
+        if (isArmstrong(number)) {
+            System.out.println(number + " is an Armstrong number.");
+        } else {
+            System.out.println(number + " is not an Armstrong number.");
+        }
+
+        scanner.close();
+    }
+
+    // Function to check Armstrong number
+    public static boolean isArmstrong(int num) {
+        int original = num;
+        int sum = 0;
+        int digits = String.valueOf(num).length();
+
+        while (num > 0) {
+            int digit = num % 10;
+            sum += Ma…
